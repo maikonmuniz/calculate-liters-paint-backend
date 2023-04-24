01b1567455d82dec1c01ac319a59f3efdf9a5d4c
maikonmuniz <maikon.muniz@yahoo.com.br>
2023-04-24 02:31:06 -0300

    feat: project complete with docker and jest and eslint

--- src/controllers/calculatePaintWall/calculatePaintWallController.js
+++ src/controllers/calculatePaintWall/calculatePaintWallController.js
@@ -10,14 +10,14 @@ const calculateLitersPaintWill = {
 			const {
 				objForCalculateAmountLiters
 			} = req.body;
-
+		
 			const arrayRuleConferred = await processValidationRule.businessRuleValidation(objForCalculateAmountLiters);
 
 			if (arrayRuleConferred.includes(false)){
-				return res.status(401).json({'Obs': 'Valor informado não é valido!'});
+				return res.status(401).json({'err': 'Valor informado não é valido!'});
 			}
 			const litersStipulator = await processCalculateLitersPaint.counterLitersStipulator(objForCalculateAmountLiters);
-
+			
 			return res.status(200).json({'result': litersStipulator});
 
 		} catch (err) {
